bad63fd1f1a19a32598fa3fd5dc8a8e6e9ec6395
EvanY <primehunter326@gmail.com>
2012-05-05 01:05:37 -0400

    Wrote a perl script to extract data
    The 1000 word training set is stored separately from the 20k word
    list that has the phonetic transcription and stuff.  Wrote a script
    to go through the training list and grep the relevant rows from the
    master list and dump those in a file.

---
+++ extract.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+
+
+use strict;
+
+
+
+my($inputHandle,$outputHandle,@data,$string,$line);
+
+open($inputHandle,"< list.dat");
+open($outputHandle,"> out.dat");
+close($outputHandle);
+
+while(<$inputHandle>){
+	$string .= $_;
+	#print $string."\n";
+}
+@data = split(/ /,$string);
+#print @data;
+for my $word (@data){
+	chomp($word);
+	$word = lc($word);
+	#print $word." \n";
+	$line = "grep \"\\b".$word."\\b[ ]*.*\" nettalk.data >> out.dat";
+	system($line);
+	print $line."\n";
+}
+
+print "Hello World!\n";
